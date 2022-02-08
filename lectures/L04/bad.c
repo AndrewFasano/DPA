75372ce1823d0f428f11f8009165c800f8a73716
Andrew Fasano <fasano@mit.edu>
2022-02-08 14:42:35 -0500

    L04 cleanup

--- lectures/L04/bad.c
+++ lectures/L04/bad.c
@@ -4,7 +4,7 @@ int main() {
     int a;
     printf("Nobody knows what this is %d\n", a);
 
-    char msg[] = {'h', 'e', 'l', 'l', 'o'};
+    char msg[] = {'h', 'e', 'l', 'l', 'o', 0};
     strcpy(&msg, "this is a bit too long");
     puts(msg);
 
